8fdde7a75fab696c14e7056162990b05f7f63f16
snehasish <snehasishdebu@gmail.com>
2024-06-28 16:53:46 +0530

    first commit

---
+++ jenkins/vars/readExcel.groovy
@@ -0,0 +1,23 @@
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.ss.usermodel.*
+
+def call(String filePath) {
+    def excelData = [:]
+    def file = new File(filePath)
+    def workbook = new XSSFWorkbook(file)
+    def sheet = workbook.getSheetAt(0)
+    def headers = sheet.getRow(0).collect { it.toString() }
+
+    sheet.each { row ->
+        if (row.getRowNum() != 0) {  // Skip header row
+            def rowData = [:]
+            headers.eachWithIndex { header, idx ->
+                rowData[header] = row.getCell(idx).toString()
+            }
+            excelData[row.getRowNum()] = rowData
+        }
+    }
+    workbook.close()
+    return excelData
+}
+
